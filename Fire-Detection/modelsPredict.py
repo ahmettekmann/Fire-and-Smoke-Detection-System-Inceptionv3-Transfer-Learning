35003cfe8b12bbdbebcb1ca6d437ecb75e440120
ahmettekman <tekman_ahmet@outlook.com>
2024-11-28 12:19:21 +0300

    InceptionV3 Transfer Learning Fire and Smoke Detection System
     Inceptionv3 modeli ile "transfer öğrenme" yöntemi kullanılarak geliştirilmiş alev ve duman tespit sistemi geliştirilmiştir.

---
+++ Fire-Detection/modelsPredict.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7c05db202f19ad9ee1a28037c772c90a1aff4e1255f024630243c43f1c499a1a
+size 1982
---
+++ Fire-Detection/trainModel.py
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:cf08447b4e0d107c936eaf60e8d199e9f5ec44ad6722898b7013289504e0fdf0
+size 2296
